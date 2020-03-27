06e142e33f2e4f28b52f15962adbbfd8216e4791
Zachary Rieck <zrr817@gmail.com>
2020-03-27 15:56:17 -0600

    Add files via upload

---
+++ module-info.java
@@ -0,0 +1,9 @@
+module covid19 {
+	
+	requires org.eclipse.draw2d;
+	requires org.eclipse.swt.win32.win32.x86_64;
+	requires org.eclipse.nebula.visualization.xygraph;
+	requires org.eclipse.nebula.visualization.widgets;
+    requires ImageIOND;
+	
+}
